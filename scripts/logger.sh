5d9df3b5967ce4b6a3c5825cfd9a53feaf8f5ecc
John Madden <jmadden173@pm.me>
2022-08-11 13:12:19 -0700

    Updated logger.sh for rocketlogger csv

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -3,7 +3,12 @@
 RL_SOCKET=/tmp/rlstream.socket
 TEROS_SOCKET=/tmp/terosstream.socket
 DEV=/dev/ttyACM0
+NUM_SAMPLES=10
 
-rocketlogger meter -r 1 -ch 0,1,2,3,4,5 -d 0 -w 0 > pipesocket $RL_SOCKET &
 teroslogger -q -s $TEROS_SOCKET /dev/ttyACM0 &
-lora
+lora &
+
+while true; do
+	rocketlogger samples $NUM_SAMPLES -r 1 -ch 0,1,2,3,4,5 -d 0 -w 0
+	csvsocket $RL_SOCKET
+done
