76d23494dd0d414ef6ef94004afefdd0150fc9ed
John Madden <jmadden173@protonmail.com>
2022-08-16 10:14:33 -0700

    Updated DATA_PATH for logger.sh

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -9,7 +9,7 @@ DEV=/dev/ttyACM0
 # Number of samples per upload
 NUM_SAMPLES=10
 # Path to log temperary data to
-DATA_PATH=/srv/rl
+DATA_PATH=/tmp/rl_data
 
 # Create data folder if dne
 if [[ ! -d $DATA_PATH ]]; then
@@ -21,7 +21,7 @@ lora $TEROS_SOCKET $RL_SOCKET &
 
 while true; do
 	TIMESTAMP=`date +"%Y-%m-%d_%T"`
-	RL_FILENAME="${TIMESTAMP}_rl.csv"
+	RL_FILENAME="${DATA_PATH}/${TIMESTAMP}_rl.csv"
 	rocketlogger sample $NUM_SAMPLES -r 1 -format csv -f $RL_FILENAME -ch 0,1,2,3,4,5 -d 0 -w 0
 	csvstream $RL_SOCKET
 done
