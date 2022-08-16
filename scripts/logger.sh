6a1e5cfb838a872bf04f6f0f57d76e0d34e2ae15
John Madden <jmadden173@protonmail.com>
2022-08-16 09:59:34 -0700

    Updated program calls in logger.sh

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -12,11 +12,11 @@ NUM_SAMPLES=10
 DATA_PATH=/srv/rl
 
 teroslogger -q -s $TEROS_SOCKET /dev/ttyACM0 &
-lora &
+lora $TEROS_SOCKET $RL_SOCKET &
 
 while true; do
 	TIMESTAMP=`date +"%Y-%m-%d_%T"`
 	RL_FILENAME="${TIMESTAMP}_rl.csv"
 	rocketlogger sample $NUM_SAMPLES -r 1 -format csv -f $RL_FILENAME -ch 0,1,2,3,4,5 -d 0 -w 0
-	csvsocket $RL_SOCKET
+	csvstream $RL_SOCKET
 done
