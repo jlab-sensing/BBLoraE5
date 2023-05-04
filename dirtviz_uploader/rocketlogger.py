910599caebd51da9c0b59a276169b5f6b1bc7283
John Madden <jmadden173@pm.me>
2023-05-04 13:16:57 -0700

    Added methods to Rocketlogger

--- dirtviz_uploader/rocketlogger.py
+++ dirtviz_uploader/rocketlogger.py
@@ -1,2 +1,21 @@
 class Rocketlogger:
-    pass
+    """Interface with Rocketlogger measurement device.
+    """
+    
+    def __init__(self):
+        """Start logging on Rocketlogger daemon and open connection to ZeroMQ
+        socket.
+        """
+
+        pass
+
+    def measure(self) -> dict:
+        """Reads most recent measurement from Rocketlogger V1, V2, I1, and I2
+        channels.
+
+        Returns
+        -------
+        dict
+            Key value pairs of measurements with the following keys ["V1", "I1",
+            "V2", "I2"].
+        """
