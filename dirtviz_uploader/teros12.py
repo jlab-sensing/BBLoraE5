9bb64ff2a614907aeb94917519936679ccde4c30
John Madden <jmadden173@pm.me>
2023-05-04 12:58:41 -0700

    Added methods to Teros12

--- dirtviz_uploader/teros12.py
+++ dirtviz_uploader/teros12.py
@@ -1,2 +1,35 @@
 class Teros12:
-    pass
+    """Interface with TEROS-12 soil moisture sensor connected through an
+    Arduino
+    """
+
+    def __init__(self, port, baud=9600):
+        """Initialize serial connection to Arduino
+
+        Parameters
+        ----------- 
+        port : str
+            Serial port of Arduino    
+        baud : int, optional
+            Baud rate of serial connection to Arduino
+        """
+
+        pass
+
+
+    def __del__(self):
+        """Close serial connection with Arduino"""
+
+        pass
+
+
+    def measure(self) -> list:
+        """Take measurement from TEROS-12
+        
+        Returns
+        -------
+        list
+            List of measurements in the following format 
+        """
+
+        pass
