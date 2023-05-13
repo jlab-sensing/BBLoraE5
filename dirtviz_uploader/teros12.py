de4a165e9c8f15ccfdcdaf8a5188709f0387225c
John Madden <jmadden173@pm.me>
2023-05-13 12:17:57 -0700

    Added parse to Teros12

--- dirtviz_uploader/teros12.py
+++ dirtviz_uploader/teros12.py
@@ -5,6 +5,32 @@ class Teros12(Serial):
     Arduino.
     """
 
+    def parse(self, raw : str) -> tuple:
+        """Parses TEROS12 data into dictionary.
+
+        Parameters
+        ----------
+        raw : str
+            Raw readings.
+
+        Returns
+        -------
+        tuple
+            Key value pairs for sensor ID and measurements in format (id, meas).
+            meas is a dictionary with keys, "VWC", "T", and "EC".
+        """
+
+        values = raw.split('+')
+        
+        data = {
+            "VWC": values[1],
+            "T": values[2],
+            "EC": values[3]
+        }
+
+        return (values[0], data)
+
+
     def measure(self) -> dict:
         """Take measurement from TEROS-12.
         
@@ -16,4 +42,25 @@ class Teros12(Serial):
             sensor.
         """
 
-        pass
+        # Measurement dictionary
+        meas = {}
+
+        # Send measure command
+        # NOTE check line endings to match Arduino implementation
+        self.write(b'MEAS')
+
+        while True:
+            # Read serial port
+            raw = self.readline()
+            raw = raw.strip()
+
+            # Check for end signal    
+            if (raw == "END"):
+                break
+
+            # Read single measurement
+            single = self.parse(raw)
+
+            meas[single[0]] = single[1]
+
+        return meas
