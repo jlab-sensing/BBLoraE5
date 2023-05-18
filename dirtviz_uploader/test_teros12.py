82bd64edf8fd4519f3db02bab6f2ff5d40b821a3
John Madden <jmadden173@pm.me>
2023-05-17 18:19:21 -0700

    Updated test_teros21.py

--- dirtviz_uploader/test_teros12.py
+++ dirtviz_uploader/test_teros12.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python
 
-import unittest
-from unittest import TestCase
+from argparse import ArgumentParser
+from time import sleep
+
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+from .rocketlogger import RocketLogger
 
 from .teros12 import Teros12
 
-TEROS12_PORT = "/dev/ttyUSB0"
-
-class TestTeros12(TestCase):
-
-	def test_init(self):
-		t12 = Teros12(TEROS12_PORT)
-
-		# Check if serial port is open
-		self.assertTrue(t12.is_open)
-
-		t12.close()
-
-	def test_measure(self):
-		with Teros12(TEROS12_PORT, 9600) as t12:
-			data = t12.measure()
-
-			# TODO Put measurement assertions her 
-
 
 if __name__ == "__main__":
-	unittest.main()
+    #
+    # Argument parser
+    # 
+
+    parser = ArgumentParser(description="Test utility to read RocketLogger measurements and print to terminal.")
+    parser.add_argument(
+        "-c", "--config",
+        required=True,
+        help="Path to config file"
+    )
+
+    args = parser.parse_args()
+    
+    # Read config file
+    with open(args.config) as s:
+        config = load(s, Loader=Loader)
+
+
+    #
+    # Read measurements
+    #
+
+    t12 = Teros12(config["teros"]["port"], config["teros"]["baud"])
+    
+    while True:
+        data = t12.measure()
+        
+        print(data)
+        
+        sleep(5)
