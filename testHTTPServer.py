fa217cd793eb157c1b9dbf1d3a4ebdfc6d8ce8cc
unknown <sgtaylor@ucsc.edu>
2023-09-28 10:59:04 -0700

    http 3

--- testHTTPServer.py
+++ testHTTPServer.py
@@ -15,6 +15,10 @@ PORT = 8000
 
 # Define a custom request handler that responds with "Hello, World!"
 class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.post_request_count = 0
+        super().__init__(*args, **kwargs)
+    
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
@@ -22,10 +26,19 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(b"Hello, World!")
         
     def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"POST request received and processed.")
+        self.post_request_count += 1
+
+        if self.post_request_count <= 5:
+            # Respond with a 200 OK for the first 5 POST requests
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"POST request received and processed.")
+        else:
+            # Respond with a 502 Bad Gateway for subsequent POST requests
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"502 Bad Gateway - Too many POST requests.")
 
 # Create the HTTP server
 with socketserver.TCPServer(("127.0.0.1", PORT), MyHandler) as httpd:
