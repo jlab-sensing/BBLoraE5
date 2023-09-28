a7123f7f3244a812b6e9b2e3fa1e8b4c379112ab
unknown <sgtaylor@ucsc.edu>
2023-09-28 11:07:51 -0700

    chat gpt fixed the error

--- testHTTPServer.py
+++ testHTTPServer.py
@@ -12,23 +12,28 @@ import socketserver
 
 # Specify the port you want to use
 PORT = 8000
+# Global counter for POST requests
+post_request_count = 0
 
-# Define a custom request handler that responds with "Hello, World!"
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        self.post_request_count = 0
-        super().__init__(*args, **kwargs)
-    
+# Define a custom request handler that responds to both GET and POST requests
+class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, World!")
-        
-    def do_POST(self):
-        self.post_request_count += 1
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, World!")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
 
-        if self.post_request_count <= 5:
+    def do_POST(self):
+        global post_request_count
+        post_request_count += 1
+
+        if post_request_count <= 5:
             # Respond with a 200 OK for the first 5 POST requests
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -36,10 +41,10 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(b"POST request received and processed.")
         else:
             # Respond with a 502 Bad Gateway for subsequent POST requests
+            self.send_response(502)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"502 Bad Gateway - Too many POST requests.")
-
 # Create the HTTP server
 with socketserver.TCPServer(("127.0.0.1", PORT), MyHandler) as httpd:
     print(f"Serving at port {PORT}")
