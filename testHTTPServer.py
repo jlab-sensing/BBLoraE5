2006ad6068fd71ec9d3688e789de5f489263c121
unknown <sgtaylor@ucsc.edu>
2023-09-28 10:42:43 -0700

    testing http

--- testHTTPServer.py
+++ testHTTPServer.py
@@ -20,6 +20,12 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Hello, World!")
+        
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"POST request received and processed.")
 
 # Create the HTTP server
 with socketserver.TCPServer(("127.0.0.1", PORT), MyHandler) as httpd:
